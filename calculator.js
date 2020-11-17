d8aa1a50be46ab1a090ee419c66f49f2aa70e6a8
bruxellesflorian <florianbertchi@gmail.com>
2020-11-17 15:14:46 +0100

    maj

--- calculator.js
+++ calculator.js
@@ -4,33 +4,27 @@ body.className = "body"
 const boxPrincipale = document.createElement("div")
 boxPrincipale.className = "flex"
 // // __________________________________________________
-
 const boxResult = document.createElement("div")
 boxResult.className = "boxResult"
-// boxResult.textContent = "0"
 const calcBoxJournal = document.createElement("div")
 calcBoxJournal.className = 'calculBasJournal'
-
 const calcBox = document.createElement("div")
 calcBox.className = "calculBas"
-
-
-
+const grosJournal = document.createElement("div")
+grosJournal.className = "putaHistorica"
 let tab = ["(", ")", "%", "AC", "7", "8", "9", "/", "4", "5", "6", "*", "1", "2", "3", "-", "0", ".", "=", "+"] // 20
-
 let flexC
-
 let button
-
 // // __________________________________________________
 
 let zoneDeCalcul = "";
 let tabZoneDeCalcul = []
 // // __________________________________________________
-
+body.appendChild(grosJournal)
 boxPrincipale.appendChild(boxResult)
 boxResult.appendChild(calcBox)
 boxResult.appendChild(calcBoxJournal)
+let pointKiller = false;
 
 for (let i = 0; i < tab.length; i++) {
     button = document.createElement('div')
@@ -46,48 +40,100 @@ for (let i = 0; i < tab.length; i++) {
         flexC = document.createElement('div')
         flexC.className = "flexC"
     }
-    
-function leCalculDelaCalculetteDeFloDeLaJournee(){
-    return  calcBox.textContent
-}
+    let touche = body.addEventListener('click', function (e) {})
 
-
+    function leCalculDelaCalculetteDeFloDeLaJournee() {
+        return calcBox.textContent
+    }
     let cases = tab[i];
-        switch(cases){
-            
-    
-            case "AC":
+    switch (cases) {
+        case "AC":
             button.addEventListener("click", function () {
+
                 calcBox.textContent = "";
                 calcBoxJournal.textContent = "";
+                pointKiller = false
             })
             break;
-
-            case "=":
-            button.addEventListener("click", function (){
+        case ".":
+            button.addEventListener("click", function () {
+                if (!pointKiller) {
+                    pointKiller = true
+                    calcBox.textContent += '.'
+                }
+            })
+            break;
+        case "/":
+            button.addEventListener("click", function () {
+                calcBox.textContent += '/'
+                calcBoxJournal.textContent += "/"
+                grosJournal.textContent += "/"
+                if (!pointKiller) {
+                    pointKiller = true
+                }
+                pointKiller = false
+            })
+            break;
+        case "*":
+            button.addEventListener("click", function () {
+                calcBox.textContent += '*'
+                calcBoxJournal.textContent += "*"
+                grosJournal.textContent += "*"
+                if (!pointKiller) {
+                    pointKiller = true
+                }
+                pointKiller = false
+            })
+            break;
+        case "-":
+            button.addEventListener("click", function () {
+                calcBox.textContent += '-'
+                calcBoxJournal.textContent += "-"
+                grosJournal.textContent += "-"
+                if (!pointKiller) {
+                    pointKiller = true
+                }
+                pointKiller = false
+            })
+            break;
+        case "+":
+            button.addEventListener("click", function () {
+                calcBox.textContent += '+'
+                calcBoxJournal.textContent += "+"
+                grosJournal.textContent += "+"
+                if (!pointKiller) {
+                    pointKiller = true
+                }
+                pointKiller = false
+            })
+            break;
+        case "=":
+            button.addEventListener("click", function () {
                 calcBox.textContent = leCalculDelaCalculetteDeFloDeLaJournee()
                 calcBoxJournal.textContent += " = "
-                calcBoxJournal.textContent += calcBox.textContent=Function('return ' + calcBox.textContent)();
+                calcBoxJournal.textContent += calcBox.textContent = Function('return ' + calcBox.textContent)();
+                grosJournal.textContent += " = "
+                grosJournal.textContent += calcBox.textContent = Function('return ' + calcBox.textContent)();
             });
             break;
-
-            default:
+        default:
             button.addEventListener("click", function () {
                 calcBox.textContent += tab[i];
                 calcBoxJournal.textContent += tab[i];
-               
-            }); 
-            
-            
-        };   
-  
-        body.appendChild(boxPrincipale)
-        boxPrincipale.appendChild(flexC)
-        flexC.appendChild(button)
-
-    
+                grosJournal.textContent += tab[i];
+            });
+    };
+    body.appendChild(boxPrincipale)
+    boxPrincipale.appendChild(flexC)
+    flexC.appendChild(button)
 }
-
-
-
-
+body.addEventListener("keypress", function(e) {
+    console.log(e.code)
+    if (e.code === "Enter") {
+        calcBox.textContent = leCalculDelaCalculetteDeFloDeLaJournee()
+        calcBoxJournal.textContent += " = "
+        calcBoxJournal.textContent += calcBox.textContent = Function('return ' + calcBox.textContent)();
+        grosJournal.textContent += " = "
+        grosJournal.textContent += calcBox.textContent = Function('return ' + calcBox.textContent)();
+    }
+})
